81a65e144e171e3d5c327bd88ca0cac61a156a07
AlisonRodrigue <alisonpereira798@gmail.com>
2025-07-31 13:43:50 -0300

    projeto finalizado

--- LogicaJS/projetos/horario/script.js
+++ LogicaJS/projetos/horario/script.js
@@ -6,10 +6,13 @@ function carregar(){
     var horas= date.getHours()
     msg.innerHTML= `Agora são ${horas} horas.`
     if(horas >= 0 && horas < 12){
-        img.src ='images/ ft-manha.png'
+        img.src ='images/ft-manha.png'
+        document.body.style.background = "#EEE3D0"
     } else if(horas >= 12 && horas < 18){
-
+        img.src= 'images/ft-tarde.png'
+        document.body.style.background="#F27405"
     } else{
-
+        img.src ='images/ft-noite.png'
+        document.body.style.background ="#1E2F31"
     }
 }
